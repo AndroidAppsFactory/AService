ac5843fa13d30994c9019f62b29a99b93d6c4544
webBasedApps <letsgithub@gmail.com>
2016-03-20 17:46:12 +0100

    commit all forgoten

--- MyApplication2/app/src/main/java/com/example/ljifar/myapplication2/MainActivity.java
+++ MyApplication2/app/src/main/java/com/example/ljifar/myapplication2/MainActivity.java
@@ -2,29 +2,23 @@ package com.example.ljifar.myapplication2;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.content.ContentValues;
 
 public class MainActivity extends Activity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 
@@ -44,4 +38,34 @@ public class MainActivity extends Activity {
         intent.setAction("com.myStaff.CUSTOM_INTENT");
         sendBroadcast(intent);
     }
+
+
+    //CONTENT PROVIDE
+    public void onClickAddName(View view) {
+        // Add a new student record
+        ContentValues values = new ContentValues();
+        values.put(StudentsProvider.NAME, ((EditText) findViewById(R.id.txtName)).getText().toString());
+        values.put(StudentsProvider.GRADE, ((EditText) findViewById(R.id.txtGrade)).getText().toString());
+        Uri uri = getContentResolver().insert(StudentsProvider.CONTENT_URI, values);
+
+        Toast.makeText(getBaseContext(), uri.toString(), Toast.LENGTH_LONG).show();
+    }
+
+    public void onClickRetrieveStudents(View view) {
+
+        // Retrieve student records
+        Uri students = Uri.parse(StudentsProvider.URL);
+        Cursor c = managedQuery(students, null, null, null, "name");
+
+        if (c.moveToFirst()) {
+            do {
+                Toast.makeText(this,
+                        c.getString(c.getColumnIndex(StudentsProvider._ID)) +
+                                ", " + c.getString(c.getColumnIndex(StudentsProvider.NAME)) +
+                                ", " + c.getString(c.getColumnIndex(StudentsProvider.GRADE)),
+                        Toast.LENGTH_SHORT).show();
+            } while (c.moveToNext());
+        }
+    }
+
 }
--- MyApplication2/app/src/main/java/com/example/ljifar/myapplication2/MyBroadcastReceiver.java
+++ MyApplication2/app/src/main/java/com/example/ljifar/myapplication2/MyBroadcastReceiver.java
@@ -3,14 +3,19 @@ package com.example.ljifar.myapplication2;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.widget.Toast;
 
 /**
  * Created by ljifar on 01/03/2016.
  */
 public class MyBroadcastReceiver extends BroadcastReceiver {
+
+    //private static final String MY_LOG_TAG = "MY_LOG_TAG";
+
     @Override
     public void onReceive(Context context, Intent intent) {
+        //Log.i(MY_LOG_TAG, "Call Placed" + intent.getDataString());
         Toast.makeText(context, "Intent Detected.", Toast.LENGTH_LONG).show();
     }
 }
